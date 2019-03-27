5d7f73bacf59d11b35247adcdd6a12f640207f94
yashasjoshi <yashasjoshi1996@gmail.com>
2019-03-27 13:25:49 -0400

    First Commit

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour {
+
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_InteractablesInContact = new List<Interactable>();
+
+	// Use this for initialization
+	private void Awake () {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        //Down
+        if (m_GrabAction.GetStateDown(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Down");
+            PickUp();
+        }
+
+        //Up
+        if (m_GrabAction.GetStateUp(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Up");
+            Drop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("interactable"))
+            return;
+
+        m_InteractablesInContact.Add(other.gameObject.GetComponent<Interactable>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("interactable"))
+            return;
+
+        m_InteractablesInContact.Remove(other.gameObject.GetComponent<Interactable>());
+    }
+
+    public void PickUp()
+    {
+        //Get the nearest interactable
+        m_CurrentInteractable = GetNearestInteractable();
+
+        //null check to see if we actually have something to pick up
+        if (!m_CurrentInteractable)
+            return;
+
+        //Check if already heald 
+        if (m_CurrentInteractable.activeHand != null)
+            m_CurrentInteractable.activeHand.Drop();
+
+        //Position it to our controller
+        m_CurrentInteractable.transform.position = transform.position;
+
+        //Attach to the fixed joint of our hands
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        m_Joint.connectedBody = targetBody;
+
+        //Set Active hand
+        m_CurrentInteractable.activeHand = this;
+    }
+
+    public void Drop()
+    {
+        //Null Check
+        if (!m_CurrentInteractable)
+            return;
+
+        //Apply Velocity
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        targetBody.velocity = m_Pose.GetVelocity();
+        targetBody.angularVelocity = m_Pose.GetAngularVelocity();
+
+        //Dettach the object from our hand
+        m_Joint.connectedBody = null;
+
+        //Clear
+        m_CurrentInteractable.activeHand = null;
+        m_CurrentInteractable = null;
+    }
+
+    private Interactable GetNearestInteractable()
+    {
+        Interactable nearest = null;
+        float minDistance = float.MaxValue;
+        float distance = 0.0f;
+
+        foreach(Interactable interactable in m_InteractablesInContact)
+        {
+            distance = (interactable.transform.position - transform.position).sqrMagnitude;
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = interactable;
+            }
+        }
+
+        return nearest;
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Teleporter : MonoBehaviour {
+
+    public GameObject m_Pointer;
+    public SteamVR_Action_Boolean m_TeleportAction;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private bool m_HasPosition = false;
+    private bool m_IsTeleporting = false;
+    private float m_FadeTime = 0.5f;
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    private void Update () {
+        //Pointer
+        m_HasPosition = UpdatePointer();
+        m_Pointer.SetActive(m_HasPosition);
+
+        //Teleport
+        if (m_TeleportAction.GetStateUp(m_Pose.inputSource))
+            TryTeleport();
+	}
+
+    private void TryTeleport()
+    {
+        //Check for valid position, and if already teleporting
+        if (!m_HasPosition || m_IsTeleporting)
+            return;
+
+        //Get CameraRig and Head Position
+        Transform cameraRig = SteamVR_Render.Top().origin;    //Basically the cameraRig Transform
+        Vector3 headPosition = SteamVR_Render.Top().head.position;
+        //Now we know where the cameraRig is, where the head is and where our pointer is pointing. So with all the information, we have to figure 
+        //out the Translation of how we are going to move the cameraRig as well as keeping the relationship between the cameraRig and the head, same.
+        //If you simple place the cameraRig to where we are pointing, the cameraRig will translate to that position but the player's head will not. (Theres gonna be a little bit of offset)
+
+        //Figure out the translation 
+        Vector3 GroundPosition = new Vector3(headPosition.x, cameraRig.position.y, headPosition.z);
+        Vector3 TranslationVector = m_Pointer.transform.position - GroundPosition;
+
+        //Move
+        StartCoroutine(MoveRig(cameraRig, TranslationVector));
+    }
+
+    private IEnumerator MoveRig(Transform cameraRig, Vector3 translation)
+    {
+        //Flagging the IsTeleporting boolean
+        m_IsTeleporting = true;
+
+        //Fade to Black
+        SteamVR_Fade.Start(Color.black, m_FadeTime, true);
+
+        //Apply Translation
+        yield return new WaitForSeconds(m_FadeTime);
+        cameraRig.position += translation;
+
+        //Clear Fade
+        SteamVR_Fade.Start(Color.clear, m_FadeTime, true);
+
+        //Toggle the Flag
+        m_IsTeleporting = false;
+    }
+
+    private bool UpdatePointer()
+    {
+        //Ray Coming from the controller
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        //If it's a hit
+        if(Physics.Raycast(ray, out hit))
+        {
+            m_Pointer.transform.position = hit.point;
+            return true;
+        }
+
+        //if not a hit
+
+        return false;
+    }
+}
